c7bd20ff87ce0dd86d92d3c2b77357db7ff9246b
NayanRaut09 <nynraut72@gmail.com>
2023-12-23 10:56:53 +0530

    Adding ArrayList with iteration

---
+++ B8-Collection/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("Nayan");
+		arrayList.add("Ram");
+		arrayList.add("Sejal");
+		arrayList.add("Sonal");
+		arrayList.add("Kiran");
+		arrayList.add("Pratik");
+		arrayList.add("Pawan");
+		
+		for(String s : arrayList) {
+			System.out.println(s);
+		}
+		
+		
+		
+		
+	}
+
+}
