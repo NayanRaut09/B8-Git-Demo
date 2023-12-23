edcdedda9fc0cb58697a1c78facb1c416f20f402
NayanRaut09 <nynraut72@gmail.com>
2023-12-23 12:03:29 +0530

    adding HashSet

---
+++ B8-Collection/src/com/demo/HashSetDemo.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+
+		 HashSet hashSet = new HashSet<>();
+		 hashSet.add(123);
+		 hashSet.add(2145);
+		 hashSet.add(110);
+		 hashSet.add(111);
+		 hashSet.add(456);
+		 hashSet.add(22);
+		 hashSet.add(789);
+		 
+		 System.out.println(hashSet);
+	}
+
+}
